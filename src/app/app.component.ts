89763357a7addbc5cfd4978b2b30876dd5cc9a7b
Dipak5202 <dipakchaudhari5202@gmail.com>
2026-01-10 14:33:01 +0530

    Anguler Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'App';
+  users = DUMMY_USERS
+
+  //handler for selected user event emitted from UserComponent (from child to parent)
+  OnSelectedUser(userId: string) {
+    console.log('Selected User Id : ', userId);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,83 @@
+import { Component, computed, EventEmitter, input, Input, output, Output, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  /**
+   * @Input decorator to receive data from parent component  (Parent to child communication)
+   * required: true  --> to make input property mandatory to be provided by parent component
+  */
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  /*
+    - EventEmitter
+    * @Output decorator to send data to parent component (Child to Parent communication)
+    - EventEmitter is used to emit events from child component to parent component
+    - creating own custom event
+  */
+  @Output() select = new EventEmitter<string>();
+
+  //On click this method will be called
+  onSelectedUser() {
+    //emitting event to parent component
+    // sendng id of selected user to parent component
+    this.select.emit(this.id);
+  }
+  get imagePath() {
+    //to get data from signal
+    return 'assets/users/' + this.avatar;
+  }
+
+
+  //======================================== Signal as Output Property ===================================================
+
+  // select = output<string>();
+
+  //======================================== Signal as Input property ===================================================
+  /* 
+    - - - - -  Using signal as Input property - - - -
+    required: true  --> to make input property mandatory to be provided by parent component
+    when we use signal as Input property, we have to call it as a function to get its value
+        Ex : this.avatar() instead of this.avatar in compponent class
+             avatar() instead of avatar in template
+    Whene ever using signal as Input property, you can not able to use Set() method to update its value
+  */
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
+  
+  //===================================== Signal and computed ======================================================
+  //
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // //Computed
+  // /* 
+
+  //   when we have to derive a value from one or more signals, we can use computed. 
+  //   It automatically tracks the signals used inside it and updates whenever any of those signals change.
+  // */
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // //getter
+  // // get imagePath() {
+  // //   //to get data from signal
+  // //   return 'assets/users/' + this.selectedUser().avatar;
+  // // }
+
+  // onSelectedUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   //setting data to signal / changing value of signal
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+}
