fd176faaa08918670402facd1221ec60440e923f
Dipak5202 <dipakchaudhari5202@gmail.com>
2026-01-10 16:45:03 +0530

    Decorators - @Input and @Output with preimatives
    Signals - signal
              siganl as input and output property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,8 +14,13 @@ export class AppComponent {
   title = 'App';
   users = DUMMY_USERS
 
+  selectedUserId = 'u1';
   //handler for selected user event emitted from UserComponent (from child to parent)
   OnSelectedUser(userId: string) {
-    console.log('Selected User Id : ', userId);
+    this.selectedUserId = userId;
+  }
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
   }
 }
